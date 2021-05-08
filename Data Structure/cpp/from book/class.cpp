9b359518ecf80208b43b130790f554cfce520bde
Sakif-Al-Faruque <sakif.saf915@gmail.com>
2021-05-08 22:13:42 +0600

    parameterized constructor & constructor overloading

--- Data Structure/cpp/from book/class.cpp
+++ Data Structure/cpp/from book/class.cpp
@@ -5,9 +5,20 @@ using namespace std;
 class myClass{
     int a;
     public:
+        myClass();
+        myClass(int); //parameterized contructor
         void set_a(int);
         int get_a();
 };
+
+//here constructor overloading has occurred
+myClass::myClass(){
+
+}
+
+myClass::myClass(int value){
+    a = value;
+}
 
 void myClass::set_a(int value){
     a = value;
@@ -21,6 +32,9 @@ int main(){
     myClass ob1;
     ob1.set_a(10);
     cout<<ob1.get_a()<<endl;
-    
+
+    myClass ob2(20);
+    cout<<ob2.get_a()<<endl;
+
     return 0;
 }
