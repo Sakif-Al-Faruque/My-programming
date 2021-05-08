2b1d451d92955b5b2c0a6a50bda816eae1d9b782
Sakif-Al-Faruque <sakif.saf915@gmail.com>
2021-05-08 22:27:37 +0600

    inheritance

--- Data Structure/cpp/from book/class.cpp
+++ Data Structure/cpp/from book/class.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 class myClass{
-    int a;
+    protected:
+        int a;  //access modifier has been assigned
     public:
         myClass();
         myClass(int); //parameterized contructor
@@ -28,6 +29,24 @@ int myClass::get_a(){
     return a;
 }
 
+//inheritence
+class myClass1:myClass{
+    int k;
+    public:
+        myClass1(int val1, int val2);
+        ~myClass1();
+};
+
+myClass1::myClass1(int val1, int val2){
+    a = val1;
+    k = val2;
+}
+
+myClass1::~myClass1(){
+    cout<<"Value of a: "<<a<<endl;
+    cout<<"Value of k: "<<k<<endl;
+}
+
 int main(){
     myClass ob1;
     ob1.set_a(10);
@@ -35,6 +54,8 @@ int main(){
 
     myClass ob2(20);
     cout<<ob2.get_a()<<endl;
+
+    myClass1 c1(30, 40);
 
     return 0;
 }
