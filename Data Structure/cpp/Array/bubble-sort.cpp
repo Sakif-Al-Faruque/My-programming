dd4f727a0bf41ed820bd8a8a451b416b5d0f4e45
Sakif-Al-Faruque <sakif.saf915@gmail.com>
2021-05-09 11:27:11 +0600

    optimized bubble sort

--- Data Structure/cpp/Array/bubble-sort.cpp
+++ Data Structure/cpp/Array/bubble-sort.cpp
@@ -3,14 +3,19 @@
 using namespace std;
 
 void bubbleSort(int arr[], int n){
-    int temp;
+    int temp, flag;
     for(int i=0; i<n-1; i++){
+        flag = 0;
         for(int j=0; j<n-1-i; j++){
             if(arr[j] > arr[j+1]){
                 temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                flag = 1;
             }
+        }
+        if(flag == 0){
+            break;
         }
     }
     cout<<"Sorted array: ";
@@ -22,7 +27,8 @@ void bubbleSort(int arr[], int n){
 
 int main(){
     int n = 5;
-    int arr [n] = {15, 16, 6, 8, 5};
+    //int arr [n] = {15, 16, 6, 8, 5};
+    int arr [n] = {16, 14, 5, 6, 8};
 
     bubbleSort(arr, n);
 }
