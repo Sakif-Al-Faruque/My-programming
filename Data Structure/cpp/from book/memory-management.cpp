6a9f64c6aadb23f764b06c44532046c65419eb1d
Sakif-Al-Faruque <sakif.saf915@gmail.com>
2021-05-16 11:47:54 +0600

    memory management

---
+++ Data Structure/cpp/from book/memory-management.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+    //for string
+    /* char str[6];
+    char *p;
+
+    cout<<"Enter a string: ";
+    cin>>str;
+
+    p = new char(strlen(str));
+    p = str;
+
+    cout<<p<<endl;
+    cout<<&p<<endl;
+    cout<<&str<<endl;
+    cout<<&str[0]<<endl;
+    cout<<p+1<<endl;
+    
+    delete[] p; */
+   
+
+    //for integer
+    /* int i, *p;
+    p = new int;
+
+    p = &i;
+
+    i=10;
+
+    cout<<*p<<endl; */
+
+    return 0; 
+}
